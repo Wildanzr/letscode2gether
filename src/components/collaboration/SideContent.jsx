80e3498f346b44c34c82ce26306032ec524c8c87
wildanzr <graita.sukma@gmail.com>
2022-09-24 11:36:29 +0700

    feat: submissions sideContent

--- src/components/collaboration/SideContent.jsx
+++ src/components/collaboration/SideContent.jsx
@@ -1,6 +1,6 @@
 import { useGlobalContext } from '../../contexts/GlobalContext'
 
-import { Problem } from '../sideContent'
+import { Submission } from '../sideContent'
 
 const SideContent = () => {
   const { globalState } = useGlobalContext()
@@ -13,14 +13,10 @@ const SideContent = () => {
         colHide
           ? 'w-full lg:w-1/2 h-full py-2 px-2'
           : 'w-full lg:w-0 h-0 lg:h-full py-0 px-0'
-      } bg-gray-300 transition-all ease-in-out duration-500`}
+      } bg-gray-300 transition-all ease-in-out duration-500 overflow-y-auto`}
     >
-      <div
-        className={`${
-          colHide ? 'flex' : 'hidden'
-        } flex-col w-full h-full overflow-y-auto`}
-      >
-        <Problem />
+      <div className={`${colHide ? 'flex' : 'hidden'} flex-col w-full h-full`}>
+        <Submission />
       </div>
     </div>
   )
