af05d6a27feed255782e19cbdbc9879054fa2baf
wildanzr <graita.sukma@gmail.com>
2022-10-28 22:35:06 +0700

    feat: register page

---
+++ src/components/form/Register.jsx
@@ -0,0 +1,187 @@
+/* eslint-disable prefer-promise-reject-errors */
+import { Form, Input, Select, DatePicker } from 'antd'
+import { Link } from 'react-router-dom'
+
+// Destructure
+const { Item } = Form
+const { Option } = Select
+
+const Register = () => {
+  const [form] = Form.useForm()
+
+  const dateFormat = 'YYYY/MM/DD'
+
+  // Finish Success
+  const onFinish = (values) => {
+    console.log(values)
+  }
+
+  // Finish Error
+  const onFinishFailed = (errorInfo) => {
+    console.log(errorInfo)
+  }
+
+  return (
+    <Form
+      name="register"
+      form={form}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      className="flex flex-col w-full duration-300 ease-in-out"
+    >
+      <Item
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!'
+          },
+          {
+            type: 'email',
+            message: 'Please input a valid email!'
+          },
+          {
+            max: 50,
+            message: 'Email must be at most 50 characters'
+          }
+        ]}
+      >
+        <Input placeholder="Email" />
+      </Item>
+
+      <Item
+        name="fullName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your full name!'
+          },
+          {
+            max: 255,
+            message: 'Full name must be at most 255 characters'
+          },
+          {
+            min: 3,
+            message: 'Full name must be at least 3 characters'
+          }
+        ]}
+      >
+        <Input placeholder="Full Name" />
+      </Item>
+
+      <Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!'
+          },
+          {
+            min: 3,
+            message: 'Username must be at least 3 characters'
+          },
+          {
+            max: 20,
+            message: 'Username must be at most 20 characters'
+          }
+        ]}
+      >
+        <Input placeholder="Username" />
+      </Item>
+
+      <Item
+        name="gender"
+        rules={[
+          {
+            required: true,
+            message: 'Please select your gender'
+          }
+        ]}
+      >
+        <Select placeholder="Gender">
+          <Option value={true}>Male</Option>
+          <Option value={false}>Female</Option>
+        </Select>
+      </Item>
+
+      <Item
+        name="dateOfBirth"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your date of birth!'
+          }
+        ]}
+      >
+        <DatePicker
+          format={dateFormat}
+          placeholder="Date of Birth"
+          className="w-full"
+        />
+      </Item>
+
+      <Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!'
+          },
+          {
+            min: 8,
+            message: 'Password must be at least 8 characters'
+          },
+          {
+            max: 50,
+            message: 'Password must be at most 50 characters'
+          }
+        ]}
+      >
+        <Input.Password placeholder="Password" />
+      </Item>
+
+      <Item
+        name="confirmPassword"
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!'
+          },
+          ({ getFieldValue }) => ({
+            validator (rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve()
+              }
+              return Promise.reject(
+                'The two passwords that you entered do not match!'
+              )
+            }
+          })
+        ]}
+      >
+        <Input.Password placeholder="Confirm Password" />
+      </Item>
+
+      <Item>
+        <button
+          type="submit"
+          className="w-full px-4 py-2 mt-4 text-sm font-medium text-center text-white font-ubuntu tracking-wider uppercase transition-colors duration-200 transform bg-easy rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
+        >
+          Register
+        </button>
+      </Item>
+
+      {/* Already have an account? login */}
+      <Item>
+        <p className="text-center font-ubuntu text-main dark:text-snow">
+          Already have an account?{' '}
+          <Link to="/auth/login" className="text-blue-500 hover:text-blue-700">
+            Login
+          </Link>
+        </p>
+      </Item>
+    </Form>
+  )
+}
+
+export default Register
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,11 +1,16 @@
-import { Navbar } from '../../layout'
+import { Navbar, Footer } from '../../layout'
+import { Register } from '../../components/form'
 
 const RegisterPage = () => {
   return (
-    <>
+    <div className='flex flex-col items-center w-full min-h-screen space-y-14 bg-snow dark:bg-main text-main dark:text-snow duration-300 ease-in-out'>
       <Navbar />
-      This is Register
-    </>
+      <div className="flex flex-col items-center w-2/3 lg:w-1/3 justify-center">
+        <p className='text-2xl lg:text-4xl text-center font-ubuntu'>Register to LetsCode</p>
+        <Register />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
