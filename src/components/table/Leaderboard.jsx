0459f1a4d1c0ea0efaee58941452371fe786be83
wildanzr <graita.sukma@gmail.com>
2022-11-10 19:36:28 +0700

    feat: leaderboard pages

---
+++ src/components/table/Leaderboard.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+import Badge1 from '../../assets/badge1.svg'
+import Badge5 from '../../assets/badge5.svg'
+import Badge6 from '../../assets/badge6.svg'
+
+const Leaderboard = () => {
+  const person = [
+    {
+      name: 'John Doe',
+      point: 1200,
+      badge: [Badge1, Badge5, Badge6]
+    },
+    {
+      name: 'Mary Jane',
+      point: 1200,
+      badge: [Badge1, Badge5, Badge6]
+    },
+    {
+      name: 'Tony Stark',
+      point: 1180,
+      badge: [Badge1, Badge5, Badge6]
+    },
+    {
+      name: 'Steve Rogers',
+      point: 1000,
+      badge: [Badge1, Badge5]
+    },
+    {
+      name: 'Bruce Banner',
+      point: 900,
+      badge: [Badge1, Badge5]
+    },
+    {
+      name: 'Thor Odinson',
+      point: 800,
+      badge: [Badge1]
+    },
+    {
+      name: 'Natasha Romanoff',
+      point: 700,
+      badge: [Badge1]
+    },
+    {
+      name: 'Clint Barton',
+      point: 600,
+      badge: [Badge1]
+    },
+    {
+      name: 'Peter Parker',
+      point: 500,
+      badge: [Badge1]
+    },
+    {
+      name: 'Wanda Maximoff',
+      point: 400,
+      badge: [Badge1]
+    }
+  ]
+  return (
+    <table className="w-full table-auto shadow-xl">
+      <thead>
+        <tr className="bg-gray-600 text-white uppercase text-sm leading-normal">
+          <th className="py-3 px-5 w-10 text-left overflow-clip whitespace-nowrap">
+            RANK
+          </th>
+          <th className="py-3 px-5 text-left overflow-clip whitespace-nowrap">
+            NAME
+          </th>
+          <th className="py-3 px-5 text-center overflow-clip whitespace-nowrap">
+            POINT
+          </th>
+          <th className="py-3 px-5 text-center overflow-clip whitespace-nowrap">
+            BADGES
+          </th>
+        </tr>
+      </thead>
+      <tbody className="text-black text-xs font-light ">
+        {person.map((item, index) => (
+          <tr
+            key={index}
+            className="border-b border-gray-200 bg-gray-50 hover:bg-gray-300"
+          >
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-start">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="ml-3 text-gray-600">{index + 1}</span>
+                </div>
+              </div>
+            </td>
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-start">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="text-gray-600">{item.name}</span>
+                </div>
+              </div>
+            </td>
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-center">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="text-gray-600">{item.point}</span>
+                </div>
+              </div>
+            </td>
+            <td className="py-1 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-center">
+                <div className="flex flex-row space-x-2 whitespace-nowrap">
+                  {item.badge.map((badge, index) => (
+                    <img src={badge} key={index} className="w-10 h-10" />
+                  ))}
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Leaderboard
--- src/pages/compete/Compete.jsx
+++ src/pages/compete/Compete.jsx
@@ -93,27 +93,31 @@ const CompetePage = () => {
         </div>
 
         <div className="flex flex-row justify-between w-full px-5 py-3 rounded-lg border-2 lg:border-4 border-easy bg-gradient-to-r from-[#CCF3F6] dark:from-[#30143F] via-[#DDCFF0] dark:via-[#151223] to:[#DCE7B3] dark:to-[#151729] duration-300 ease-out">
-          <div className="flex w-4/6 flex-col">
-            <p className='mb-0 font-ubuntu text-3xl tracking-wide font-medium'>UAP PEMDAS - FILKOM 2022</p>
-            <div className="flex flex-col w-full space-y-2 items-start">
-              <p className='mb-0 text-sm font-ubuntu font-medium'>
+          <div className="flex w-full flex-col space-y-3">
+
+            {/* Compete Title and Button Join */}
+            <div className="flex flex-row w-full items-center justify-between">
+              <p className='mb-0 font-ubuntu text-3xl tracking-wide font-medium'>UAP PEMDAS - FILKOM 2022</p>
+              <button className='px-2 py-2 bg-main dark:bg-alternate text-snow dark:text-snow whitespace-nowrap rounded font-medium'>Join Now</button>
+            </div>
+
+            {/* Compete Information */}
+            <div className="flex flex-col w-full space-y-1 items-start">
+              <p className='mb-0 text-sm font-ubuntu font-bold'>
                 Challenger: <span className='pl-2 mb-0 text-sm font-ubuntu font-thin text-easy'>Mr. John Doe</span>
               </p>
-              <p className='mb-0 text-sm font-ubuntu font-medium'>
+              <p className='mb-0 text-sm font-ubuntu font-bold'>
                 Starts In: <span className='mb-0 text-sm font-ubuntu font-thin'>Wednesday, 24 May 2022 14:00</span>
               </p>
-              <p className='mb-0 text-sm font-ubuntu font-medium'>
+              <p className='mb-0 text-sm font-ubuntu font-bold'>
                 Ends In: <span className='pl-2 mb-0 text-sm font-ubuntu font-thin'>Wednesday, 24 May 2022 16:00</span>
               </p>
               <p className='mb-0 text-sm font-ubuntu font-thin text-justify'>
-                <span className='font-medium'>Description:</span> <br />
+                <span className='font-bold'>Description:</span> <br />
                 Praesent gravida nisi velit, eu gravida nunc ultrices nec. Donec ut rutrum lacus, quis imperdiet urna. Cras mollis lectus eu neque dictum, at mollis enim tristique. Curabitur convallis nulla mauris, vitae posuere tortor scelerisque tristique. Aliquam pellentesque justo in scelerisque commodo. Integer urna leo, elementum et nisi nec, fringilla efficitur dui. Integer placerat justo semper nisi imperdiet suscipit. Ut consectetur, eros non molestie tempus, eros lectus blandit enim, at pellentesque arcu eros sit amet dolor. Nam eget justo sem.
               </p>
             </div>
-          </div>
 
-          <div className="flex w-2/6 items-start justify-end">
-            <button className='w-full lg:w-2/3 py-2 bg-main dark:bg-alternate text-snow dark:text-snow rounded font-medium'>Join Now</button>
           </div>
         </div>
       </div>
