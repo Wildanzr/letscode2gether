33e8e9fc453adc0143901a97b7537bb974e7b13a
wildanzr <graita.sukma@gmail.com>
2022-11-12 06:47:39 +0700

    feat: popular competes dummy

---
+++ src/components/table/PopularCompete.jsx
@@ -0,0 +1,49 @@
+const PopularCompete = (props) => {
+  const { competes } = props
+  return (
+    <table className="w-full table-auto shadow-xl">
+      <thead>
+        <tr className="bg-gray-600 text-white uppercase text-sm leading-normal">
+          <th className="py-3 px-5 w-[5%] text-left overflow-clip whitespace-nowrap">
+            NO
+          </th>
+          <th className="py-3 px-5 w-[85%] text-left overflow-clip whitespace-nowrap">
+            NAME
+          </th>
+          <th className="py-3 px-5 w-[10%] text-right overflow-clip whitespace-nowrap">
+            PARTICIPANTS
+          </th>
+        </tr>
+      </thead>
+      <tbody className="text-black text-xs font-light ">
+        {competes.map((compete, index) => (
+            <tr key={index} className="border-b border-gray-200 bg-gray-50 hover:bg-gray-300">
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-start">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="ml-3 text-gray-600">{index + 1}</span>
+                </div>
+              </div>
+            </td>
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-start">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="text-gray-600">{compete.name}</span>
+                </div>
+              </div>
+            </td>
+            <td className="py-3 px-5 text-left overflow-clip">
+              <div className="flex items-center justify-center">
+                <div className="font-medium whitespace-nowrap">
+                  <span className="text-gray-600">{compete.participants}</span>
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default PopularCompete
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,13 +1,47 @@
-import { Navbar, Footer } from '../../layout'
+import { useState } from 'react'
 
+import { Navbar, Footer } from '../../layout'
 import { Statistics } from '../../views'
+import { PopularCompete } from '../../components/table'
 
 const DashboardPage = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [competes, setCompetes] = useState([
+    {
+      name: 'Live Coding - PEMDAS 2022',
+      participants: 135
+    },
+    {
+      name: 'Hack The Box',
+      participants: 120
+    },
+    {
+      name: 'Latihan Soal - UTP 2022',
+      participants: 96
+    },
+    {
+      name: 'Live Coding - ASD 2022',
+      participants: 86
+    },
+    {
+      name: 'Live Coding - PBO 2022',
+      participants: 74
+    }
+  ])
   return (
     <div className='flex flex-col items-center justify-between w-full min-h-screen space-y-14 bg-snow dark:bg-main text-main dark:text-snow duration-300 ease-in-out'>
       <Navbar />
-      <div className="flex flex-col w-11/12">
+      <div className="flex flex-col w-11/12 space-y-10">
         <Statistics />
+
+        <div className="flex flex-col space-y-4">
+          <h3 className='mb-0 font-ubuntu text-main dark:text-snow text-xl font-medium duration-300 ease-in-out'>Popular Competes</h3>
+          <div className="flex flex-col py-4 overflow-y-auto">
+            <div className="flex w-full">
+              <PopularCompete competes={competes} />
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
