e9f4e025b3c6714e9883463bd35c58a328bfc17a
wildanzr <graita.sukma@gmail.com>
2022-11-15 22:06:05 +0700

    feat: description detail on journey

--- src/components/other/Description.jsx
+++ src/components/other/Description.jsx
@@ -6,9 +6,9 @@ const Description = (props) => {
         <p className="mb-0 font-medium text-base">{title}</p>
       </div>
       <div className="flex w-3/4">
-        <p className="mb-0 font-light text-sm text-justify">
+        <div className="mb-0 font-light text-sm text-justify">
           {value}
-        </p>
+        </div>
       </div>
     </div>
   )
--- src/pages/admin/DetailJourney.jsx
+++ src/pages/admin/DetailJourney.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { languageOptions } from '../../constants/languageOptions'
 
+import api from '../../api'
 import { Navbar, Footer } from '../../layout'
 import { Breadcrumb } from '../../components/breadcrumb'
 import { ListOfProblem } from '../../components/table'
 import { Description } from '../../components/other'
 
+import Cookies from 'js-cookie'
+import { Skeleton, Tag } from 'antd'
 import { useParams } from 'react-router-dom'
 
 const DetailJourneyPage = () => {
@@ -22,6 +26,58 @@ const DetailJourneyPage = () => {
       target: `/admin/manage/journeys/${journeyId}`
     }
   ])
+
+  // Local states
+  const [journeyDetail, setJourneyDetail] = useState(null)
+  const [languageAllowed, setLanguageAllowed] = useState(null)
+
+  // Get detail of journey
+  const getDetailJourney = async () => {
+    // Configuration
+    const config = {
+      headers: {
+        Authorization: `Bearer ${Cookies.get('jwtToken')}`
+      }
+    }
+
+    try {
+      const { data } = await api.get(`/competes/${journeyId}`, config)
+      const { compete } = data.data
+
+      setJourneyDetail(compete)
+      setLanguageAllowed(transformLanguageAllowed(compete.languageAllowed))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Transform language allowed
+  const transformLanguageAllowed = (selected) => {
+    const languageAllowed = languageOptions
+      .filter(lang => selected.includes(lang.id))
+      .map(lang => lang.label)
+
+    return (
+      <>
+        {languageAllowed.map((lang, index) => (
+          <Tag
+            key={index}
+            color="default"
+            className='font-ubuntu font-medium'
+          >
+            <span className='text-easy'>
+              {lang}
+            </span>
+          </Tag>
+        ))}
+      </>
+    )
+  }
+
+  // Initial get detail of journey
+  useEffect(() => {
+    getDetailJourney()
+  }, [])
   return (
     <div className="flex flex-col items-center justify-between w-full min-h-screen space-y-14 bg-snow dark:bg-main text-main dark:text-snow duration-300 ease-in-out">
       <Navbar>
@@ -35,24 +91,36 @@ const DetailJourneyPage = () => {
           </div>
 
           {/* Detail of Journey */}
-          <div className="flex flex-col space-y-4 w-full font-ubuntu">
-            <Description title="Name" value="Section 1 - Input Output" />
-            <Description
-              title="Description"
-              value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet aliquet lacinia, nisl nisl aliquet nisl, eget aliquet nunc nisl eget nisl. Sed euismod, nunc sit amet aliquet lacinia, nisl nisl aliquet nisl, eget aliquet nunc nisl eget nisl."
-            />
-            <Description title="Language Allowed" value="C++" />
-            <Description title="Problems" value="" />
+          {
+            journeyDetail
+              ? (
+              <div className="flex flex-col space-y-4 w-full font-ubuntu">
+                <Description title="Name" value={journeyDetail.name} />
+                <Description
+                  title="Description"
+                  value={journeyDetail.description}
+                />
+                <Description title="Language Allowed" value={languageAllowed} />
+                <Description title="Problems" value="" />
 
-            {/* List of Problems */}
-            <div className="flex flex-col w-full space-y-2 overflow-y-auto">
-              <div className="flex flex-col pb-4 overflow-y-auto">
-                <div className="flex w-full">
-                  <ListOfProblem />
+                {/* List of Problems */}
+                <div className="flex flex-col w-full space-y-2 overflow-y-auto">
+                  <div className="flex flex-col pb-4 overflow-y-auto">
+                    <div className="flex w-full">
+                      <ListOfProblem />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+                )
+              : (
+              <Skeleton
+                active
+                paragraph={{ rows: 4 }}
+              />
+                )
+          }
+
         </div>
       </Navbar>
 
