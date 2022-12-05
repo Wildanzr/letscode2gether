c66da793ff9cdd135288c3e063b3e49ce4b07796
wildanzr <graita.sukma@gmail.com>
2022-12-05 20:36:24 +0700

    feat: overall leaderboard

--- src/components/table/Leaderboard.jsx
+++ src/components/table/Leaderboard.jsx
@@ -1,62 +1,35 @@
 import React from 'react'
 
-import Badge1 from '../../assets/badge1.svg'
-import Badge5 from '../../assets/badge5.svg'
-import Badge6 from '../../assets/badge6.svg'
+import B1 from '../../assets/badge1.svg'
+import B2 from '../../assets/badge2.svg'
+import B3 from '../../assets/badge3.svg'
+import B4 from '../../assets/badge4.svg'
+import B5 from '../../assets/badge5.svg'
+import B6 from '../../assets/badge6.svg'
 
-const Leaderboard = () => {
-  const person = [
-    {
-      name: 'John Doe',
-      point: 1200,
-      badge: [Badge1, Badge5, Badge6]
-    },
-    {
-      name: 'Mary Jane',
-      point: 1200,
-      badge: [Badge1, Badge5, Badge6]
-    },
-    {
-      name: 'Tony Stark',
-      point: 1180,
-      badge: [Badge1, Badge5, Badge6]
-    },
-    {
-      name: 'Steve Rogers',
-      point: 1000,
-      badge: [Badge1, Badge5]
-    },
-    {
-      name: 'Bruce Banner',
-      point: 900,
-      badge: [Badge1, Badge5]
-    },
-    {
-      name: 'Thor Odinson',
-      point: 800,
-      badge: [Badge1]
-    },
-    {
-      name: 'Natasha Romanoff',
-      point: 700,
-      badge: [Badge1]
-    },
-    {
-      name: 'Clint Barton',
-      point: 600,
-      badge: [Badge1]
-    },
-    {
-      name: 'Peter Parker',
-      point: 500,
-      badge: [Badge1]
-    },
-    {
-      name: 'Wanda Maximoff',
-      point: 400,
-      badge: [Badge1]
+const Leaderboard = (props) => {
+  // Destructure props
+  const { leaderboard } = props
+
+  // Define badge
+  const defineBadge = (point) => {
+    if (point === 0) {
+      return <p className='mb-0 text-2xl font-ubuntu font-bold text-success'>-</p>
+    } else if (point < 500) {
+      return <img src={B1} className="w-10" />
+    } else if (point < 1000) {
+      return <img src={B2} className="w-10" />
+    } else if (point < 1500) {
+      return <img src={B3} className="w-10" />
+    } else if (point < 2000) {
+      return <img src={B4} className="w-10" />
+    } else if (point < 2500) {
+      return <img src={B5} className="w-10" />
+    } else {
+      return <img src={B6} className="w-10" />
     }
-  ]
+  }
+
   return (
     <table className="w-full table-auto shadow-md">
       <thead>
@@ -71,12 +44,12 @@ const Leaderboard = () => {
             POINT
           </th>
           <th className="py-3 px-5 text-center overflow-clip whitespace-nowrap">
-            BADGES
+            CURRENT BADGE
           </th>
         </tr>
       </thead>
       <tbody className="text-black text-xs font-light ">
-        {person.map((item, index) => (
+        {leaderboard.map((person, index) => (
           <tr
             key={index}
             className="border-b border-gray-200 bg-gray-100 hover:bg-white"
@@ -91,23 +64,21 @@ const Leaderboard = () => {
             <td className="py-3 px-5 text-left overflow-clip">
               <div className="flex items-center justify-start">
                 <div className="font-medium whitespace-nowrap">
-                  <span className="text-gray-600">{item.name}</span>
+                  <span className="text-gray-600">{person.username}</span>
                 </div>
               </div>
             </td>
             <td className="py-3 px-5 text-left overflow-clip">
               <div className="flex items-center justify-center">
                 <div className="font-medium whitespace-nowrap">
-                  <span className="text-gray-600">{item.point}</span>
+                  <span className="text-gray-600">{person.point}</span>
                 </div>
               </div>
             </td>
             <td className="py-1 px-5 text-left overflow-clip">
               <div className="flex items-center justify-center">
                 <div className="flex flex-row space-x-2 whitespace-nowrap">
-                  {item.badge.map((badge, index) => (
-                    <img src={badge} key={index} className="w-10 h-10" />
-                  ))}
+                  {defineBadge(person.point)}
                 </div>
               </div>
             </td>
