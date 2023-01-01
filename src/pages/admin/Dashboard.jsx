1589c5b9e661dede6b2776638e5b610d6d583862
wildanzr <graita.sukma@gmail.com>
2023-01-01 13:58:09 +0700

    fix: leaderboard on dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,76 +1,55 @@
-import { useState } from 'react'
+import langConfig from '../../config/langConfig.json'
+import { useState, useEffect } from 'react'
+
+import api from '../../api'
 
 import { Navbar, Footer } from '../../layout'
 import { Statistics } from '../../views'
-import { PopularCompete, PopularProblem } from '../../components/table'
+import { Leaderboard } from '../../components/table'
+
+import { Spin } from 'antd'
 
 const DashboardPage = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [competes, setCompetes] = useState([
-    {
-      name: 'Live Coding - PEMDAS 2022',
-      participants: 135
-    },
-    {
-      name: 'Hack The Box',
-      participants: 120
-    },
-    {
-      name: 'Latihan Soal - UTP 2022',
-      participants: 96
-    },
-    {
-      name: 'Live Coding - ASD 2022',
-      participants: 86
-    },
-    {
-      name: 'Live Coding - PBO 2022',
-      participants: 74
+  // Local States
+  const [leaderboard, setLeaderboard] = useState(null)
+
+  // Get overall leaderboard
+  const getOverallLeaderboard = async () => {
+    try {
+      const { data } = await api.get('/competes/overall-leaderboard')
+      const { data: leaderboard } = data
+      // console.log(data)
+
+      // Set Value
+      setLeaderboard(leaderboard)
+    } catch (error) {
+      console.log(error)
     }
-  ])
-  // eslint-disable-next-line no-unused-vars
-  const [problems, setProblems] = useState([
-    {
-      name: 'Match The Pattern',
-      submissions: 390,
-      rate: 0.344
-    },
-    {
-      name: 'The Greatest Common Divisor',
-      submissions: 385,
-      rate: 0.675
-    },
-    {
-      name: 'Wave Sort',
-      submissions: 361,
-      rate: 0.313
-    }
-  ])
+  }
+
+  // Initially get overall leaderboard
+  useEffect(() => {
+    getOverallLeaderboard()
+  }, [])
   return (
     <div className="flex flex-col items-center justify-between w-full min-h-screen space-y-14 bg-snow dark:bg-main text-main dark:text-snow duration-300 ease-in-out">
       <Navbar>
         <div className="flex flex-col w-11/12 space-y-6">
           <Statistics />
 
-          <div className="flex flex-col space-y-4">
-            <h3 className="mb-0 font-ubuntu text-main dark:text-snow text-xl font-medium duration-300 ease-in-out">
-              Popular Competes
-            </h3>
-            <div className="flex flex-col py-4 overflow-y-auto">
-              <div className="flex w-full">
-                <PopularCompete competes={competes} />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <h3 className="mb-0 font-ubuntu text-main dark:text-snow text-xl font-medium duration-300 ease-in-out">
-              Popular Competes
-            </h3>
-            <div className="flex flex-col py-4 overflow-y-auto">
-              <div className="flex w-full">
-                <PopularProblem problems={problems} />
-              </div>
+          {/* Leaderboard */}
+          <h3 className='mb-0 font-ubuntu text-main dark:text-snow text-xl font-medium duration-300 ease-in-out'>
+            {langConfig.dashboardLeaderboards}
+          </h3>
+          <div className="flex w-full items-center justify-center pb-5 overflow-y-auto">
+            <div className="flex flex-col w-full items-center justify-center">
+              {leaderboard === null
+                ? (
+                <Spin size="default" />
+                  )
+                : (
+                <Leaderboard leaderboard={leaderboard} />
+                  )}
             </div>
           </div>
         </div>
