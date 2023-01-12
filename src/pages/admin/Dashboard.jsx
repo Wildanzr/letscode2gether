c75c1d33604fa48650893d0f5238438d253a217a
wildanzr <graita.sukma@gmail.com>
2023-01-12 13:03:33 +0700

    feat: update dynamic statistic

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -8,10 +8,12 @@ import { Statistics } from '../../views'
 import { Leaderboard } from '../../components/table'
 
 import { Spin } from 'antd'
+import Cookies from 'js-cookie'
 
 const DashboardPage = () => {
   // Local States
   const [leaderboard, setLeaderboard] = useState(null)
+  const [statistic, setStatistic] = useState(null)
 
   // Get overall leaderboard
   const getOverallLeaderboard = async () => {
@@ -27,15 +29,50 @@ const DashboardPage = () => {
     }
   }
 
+  // Get dashboard statistic
+  const getDashboardStatistic = async () => {
+    // config
+    const config = {
+      headers: {
+        authorization: `Bearer ${Cookies.get('jwtToken')}`
+      }
+    }
+
+    try {
+      const { data } = await api.get('/competes/statistics', config)
+      console.log(data)
+
+      // Destructure
+      const { totalCompetes, totalProblems, totalStudent, totalSubmissions, totalTeacher } = data.data
+
+      // Set Value
+      setStatistic({
+        totalCompetes,
+        totalProblems,
+        totalStudent,
+        totalSubmissions,
+        totalTeacher
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   // Initially get overall leaderboard
   useEffect(() => {
     getOverallLeaderboard()
+    getDashboardStatistic()
   }, [])
   return (
     <div className="flex flex-col items-center justify-between w-full min-h-screen space-y-14 bg-snow dark:bg-main text-main dark:text-snow duration-300 ease-in-out">
       <Navbar>
         <div className="flex flex-col w-11/12 space-y-6">
-          <Statistics />
+          <Statistics
+            statistic={statistic === null
+              ? null
+              : statistic
+            }
+          />
 
           {/* Leaderboard */}
           <h3 className='mb-0 font-ubuntu text-main dark:text-snow text-xl font-medium duration-300 ease-in-out'>
