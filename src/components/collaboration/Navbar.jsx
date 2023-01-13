a8d3fd16934db9ff498360a2810709afc7c2b219
wildanzr <graita.sukma@gmail.com>
2023-01-13 10:22:59 +0700

    feat: customize

--- src/components/collaboration/Navbar.jsx
+++ src/components/collaboration/Navbar.jsx
@@ -34,10 +34,10 @@ const Navbar = () => {
     }
   }
   return (
-    <div className="flex flex-row lg:flex-col w-full lg:w-[10%] h-[7%] lg:h-full font-ubuntu py-2 px-2 z-40 items-center">
+    <div className="flex flex-row lg:flex-col w-full lg:w-[10%] h-[7%] lg:h-full font-ubuntu py-2 px-2 z-40 items-center justify-center">
       <div className="flex flex-row lg:flex-col w-5/6 lg:w-full space-x-4 lg:space-y-4 lg:space-x-0 lg:mt-5 items-center justify-center text-snow">
         <button
-          className="flex flex-row group space-x-1 items-center py-1 px-2 lg:w-full justify-center rounded-sm text-main dark:text-snow font-bold border-2 border-main dark:border-snow hover:text-easy hover:border-easy dark:hover:border-easy duration-300 ease-in-out"
+          className="hidden lg:flex flex-row group space-x-1 items-center py-1 px-2 lg:w-full justify-center rounded-sm text-main dark:text-snow font-bold border-2 border-main dark:border-snow hover:text-easy hover:border-easy dark:hover:border-easy duration-300 ease-in-out"
           onClick={() => navigate(-1)}
         >
           <BsCaretLeft className='fill-main dark:fill-snow w-5 h-5 duration-300 ease-in-out group-hover:fill-easy'/>
