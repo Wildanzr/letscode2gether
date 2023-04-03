3eb7f01a98ab8adaceb180ccc52fc09115e686ac
wildanzr <graita.sukma@gmail.com>
2023-04-03 17:40:30 +0700

    update: local build

--- dist/assets/liquid.cab97955.js
+++ dist/assets/liquid.cab97955.js
@@ -1,4 +1,4 @@
-import{m as d}from"./index.9523839c.js";import"./react-dom.59e1c638.js";import"./sweetalert2-react-content.d34d26cf.js";import"./axios.f1a89be8.js";import"./antd.1feda85b.js";import"./react-router-dom.938a24ae.js";import"./sweetalert2.13d284bd.js";import"./react-icons.c47407cf.js";import"./framer-motion.7a592db3.js";import"./react-draggable.01f97173.js";/*!-----------------------------------------------------------------------------
+import{m as d}from"./index.28cff3ad.js";import"./react-dom.59e1c638.js";import"./sweetalert2-react-content.d34d26cf.js";import"./axios.f1a89be8.js";import"./antd.1feda85b.js";import"./react-router-dom.938a24ae.js";import"./sweetalert2.13d284bd.js";import"./react-icons.c47407cf.js";import"./framer-motion.7a592db3.js";import"./react-draggable.01f97173.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.0(9d278685b078158491964f8fd7ac9628fffa0f30)
  * Released under the MIT license
